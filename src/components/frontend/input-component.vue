dc8264969faf2f649fa3c8720b5db144c3e440ba
Olek <94283587+olek-program@users.noreply.github.com>
2023-02-02 15:53:49 +0100

    Todo list system introuced

---
+++ src/components/frontend/input-component.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="input-componemt">
+    <input v-model="text" placeholder="..." />
+    <button @click="addToList">Add</button>
+    <div class="list">{{ score }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      text: "",
+    };
+  },
+  computed: {
+    score() {
+      return `Added to: %{this.lista}`;
+    },
+  },
+  methods: {
+    addToList() {
+      this.list.push(this.text);
+    },
+  },
+};
+</script>
